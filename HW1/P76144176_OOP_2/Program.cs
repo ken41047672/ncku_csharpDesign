1a10c1e632abdadc2ef92ee4710f9d1ff2465325
ken41047672 <129359567+ken41047672@users.noreply.github.com>
2025-11-10 16:57:36 +0800

    Add files via upload

---
+++ HW1/P76144176_OOP_2/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace P76144176_OOP_2
+{
+    internal class Program
+    {
+        class Workpiece
+        {
+            public Workpiece(int _id)
+            {
+                id = _id;
+                isPassed = false;
+            }
+            int id;
+            bool isPassed;
+            public int Id { get { return id;  } }
+            public bool IsPassed { get { return isPassed;  } set { isPassed = value; } }
+        }
+
+
+        class ProductionLine
+        {
+            public ProductionLine()
+            {
+                workpieces = new List<Workpiece>();
+            }
+            List<Workpiece> workpieces;
+            public void AddWorkpiece(Workpiece toAdd)
+            {
+                workpieces.Add(toAdd);
+            }
+            public void RunQualityControl(IQualityControlStrategy qcStrategy)
+            {
+                int passed = 0, failed = 0;
+                Console.WriteLine("--- 開始執行品質檢測 ---");
+                foreach(Workpiece piece in workpieces)
+                {
+                    Console.Write($"對工件 [{ piece.Id}] ");
+                    bool result = qcStrategy.PerformCheck(piece);   
+
+                    if (result)
+                    {
+                        Console.WriteLine(" 通過!");
+                        piece.IsPassed = true;
+                        passed++;
+                    }
+                    else
+                    {
+                        Console.WriteLine(" 未通過。");
+                        piece.IsPassed = false;
+                        failed++;
+                    }
+                }
+                Console.WriteLine("--- 檢測完成 ---");
+                Console.WriteLine($"檢測結果: {passed}個通過，{failed}個失敗");
+
+            }
+        }
+        interface IQualityControlStrategy
+        {
+            public bool PerformCheck(Workpiece item);
+        }
+        class VisualInspection : IQualityControlStrategy
+        {
+            public bool PerformCheck(Workpiece item)
+            {
+                Console.Write("執行視覺檢測...");
+                return true;
+            }
+        }
+        class ElectronicTest : IQualityControlStrategy
+        {
+            public bool PerformCheck(Workpiece item)
+            {
+                Console.Write("執行精密電子測試...");
+                if (item.Id % 2 == 0) return true;
+                else                  return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 3;
+            ProductionLine p = new ProductionLine();
+            p.AddWorkpiece(new Workpiece(101));
+            p.AddWorkpiece(new Workpiece(102));
+            p.AddWorkpiece(new Workpiece(103));
+            Console.WriteLine($"生產線上準備了 {n} 個帶檢測工件 (ID: 101, 102, 103)");
+            Console.WriteLine("請選擇檢測策略: [1] 視覺檢測 [2] 精密電子測試");
+            Console.Write("> ");
+            int op = 0;
+            try
+            {
+                string input = Console.ReadLine();
+                op = int.Parse(input);
+            }
+            catch (Exception ex) { }
+            if (op == 1)
+            {
+                VisualInspection v = new VisualInspection();
+                p.RunQualityControl(v);
+            }
+            else if (op == 2)
+            {
+                ElectronicTest v = new ElectronicTest();
+                p.RunQualityControl(v);
+            }
+        }
+    }
+}
---
+++ HW1/P76144176_OOP_3/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace P76144176_OOP_3
+{
+    internal class Program
+    {
+        public enum MachineType{ CNC, RoboticArm,  Conveyor}
+        public enum MachineStatus{ Running, Idle, Error}
+
+        public class Machine
+        {
+            public Machine(string id, MachineType type, MachineStatus status, List<int> errorCodes)
+            {
+                Id = id;
+                Type = type;
+                Status = status;
+                ErrorCodes = errorCodes;
+            }
+
+            public string Id { get; set; }
+            public MachineType Type { get; set; }
+            public MachineStatus Status { get; set; }
+            public List<int> ErrorCodes { get; set; } = new List<int>();
+        }
+
+        public class FactoryFloor
+        {
+            public string FloorName { get; set; }
+            private List<Machine> machines = new List<Machine>();
+
+            public FactoryFloor()
+            {
+            }
+
+            public void AddMachine(Machine machine)
+            {
+                machines.Add(machine);
+            }
+
+            public List<Machine> GetMachinesByStatus(MachineStatus status)
+            {
+                List<Machine> result = new List<Machine>();
+                foreach (Machine machine in machines) {
+                    if (machine.Status == status)
+                    {
+                        result.Add(machine);
+                    }
+                }
+                return result;
+            }
+
+            public Machine FindMachineWithMostErrors()
+            {
+                return machines.OrderByDescending(m => m.ErrorCodes.Count).FirstOrDefault();
+            }
+
+            
+        }
+        static void Main(string[] args)
+        {
+            FactoryFloor f = new FactoryFloor();
+            f.AddMachine(new Machine("A01", MachineType.CNC, MachineStatus.Running, new List<int>()));
+            f.AddMachine(new Machine("B02", MachineType.RoboticArm, MachineStatus.Running, new List<int>()));
+            f.AddMachine(new Machine("A03", MachineType.RoboticArm, MachineStatus.Error, new List<int> { 1, 2, 3, 4, 5 }));
+            while (true)
+            {
+                Console.WriteLine("--- 機台監控儀表板 ---");
+                Console.WriteLine("[1] 依狀態篩選機台");
+                Console.WriteLine("[2] 尋找錯誤最多的機台");
+                Console.WriteLine("[3] 離開");
+                Console.Write("> ");
+                int op = int.Parse(Console.ReadLine());
+                if (op == 1)
+                {
+                    Console.Write("請輸入要查詢的狀態 (Running, Idle, Error): ");
+                    string type = Console.ReadLine();
+                    Console.WriteLine();
+                    List<Machine> result = null;
+                    if(type == "Running")
+                    {
+                        Console.WriteLine("--- 狀態為 [Running] 的機台: ---");
+                        result = f.GetMachinesByStatus(MachineStatus.Running);
+       
+                    }
+                    else if(type == "Idle")
+                    {
+
+                        Console.WriteLine("--- 狀態為 [Idle] 的機台: ---");
+                        result = f.GetMachinesByStatus(MachineStatus.Idle);
+                    }
+                    else if(type == "Error")
+                    {
+                        Console.WriteLine("--- 狀態為 [Error] 的機台: ---");
+
+                        result = f.GetMachinesByStatus(MachineStatus.Error);
+                    }
+
+                    foreach(Machine m in result)
+                    {
+                        Console.WriteLine($"- 機台ID: {m.Id}, 類型: {m.Type}, 錯誤數: {m.ErrorCodes.Count}");
+                    }
+                    Console.WriteLine();
+                }
+                else if (op == 2)
+                {
+                    Console.WriteLine();
+                    Machine m = f.FindMachineWithMostErrors();
+                    Console.WriteLine($"錯誤最多的機台是: 機台ID: {m.Id} ({m.Type})， 共有 {m.ErrorCodes.Count} 個錯誤紀錄");
+                    Console.WriteLine();
+                }
+                else if (op == 3)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
